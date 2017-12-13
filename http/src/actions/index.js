309f65393a8028be7bc5348d2d49fa5cac913529
Vinnie <vinnie.scalco@gmail.com>
2017-12-13 11:09:27 -0600

    Initial commit

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,11 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    const friendsEndpoint = 'http://localhost:5000/friends';
+    const friends = axios.get(friendsEndpoint);
+  
+    return {
+      type: GET_FRIENDS,
+      payload: friends
+    };
 };
